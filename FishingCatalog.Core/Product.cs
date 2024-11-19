7ded51739b8417881995b9a0b4621913fc42ac3f
Mikhail Kapytok <mishakopytok@gmail.com>
2024-11-19 19:20:04 +0300

    added models

--- FishingCatalog.Core/Product.cs
+++ FishingCatalog.Core/Product.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace FishingCatalog.Core
 {
-    internal class Product
+    public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public byte[] Image { get; set; }
     }
 }
--- FishingCatalog.Core/User.cs
+++ FishingCatalog.Core/User.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace FishingCatalog.Core
 {
-    internal class User
+    public class User
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastLogin { get; set;}
+        public bool IsActive { get; set; }
+        public int RoleId { get; set; }
     }
 }
