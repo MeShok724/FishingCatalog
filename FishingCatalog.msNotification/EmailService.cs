f4827137be91085601c36d1a9f40626f0333bfd5
Mikhail Kapytok <mishakopytok@gmail.com>
2024-12-01 16:51:43 +0300

    Notification service

---
+++ FishingCatalog.msNotification/EmailService.cs
@@ -0,0 +1,31 @@
+﻿using MimeKit;
+using MailKit.Net.Smtp;
+using System.Threading.Tasks;
+
+namespace FishingCatalog.msNotification
+{
+    public class EmailService
+    {
+        public static async Task SendEmailAsync(string email, string subject, string message)
+        {
+            using var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress("Администрация сайта", "mishakopytok@gmail.com"));
+            emailMessage.To.Add(new MailboxAddress("", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.gmail.com", 465, true);
+
+                await client.AuthenticateAsync("mishakopytok@gmail.com", "arxe oqmp ohhv zkmz");
+
+                await client.SendAsync(emailMessage);
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
---
+++ FishingCatalog.msNotification/Program.cs
@@ -0,0 +1,6 @@
+using FishingCatalog.msNotification;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.Run();
